bc9ef13682c2744bfa44d8cfcbc6a285b9ffcdbf
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-21 17:06:11 +0530

    "get Token after sign up"

--- functions/index.js
+++ functions/index.js
@@ -74,6 +74,7 @@ app.post('/signup', (req, res) => {
     };
 
     // TODO validate data
+    let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
         .then(doc => {
             if(doc.exists) {
@@ -85,14 +86,29 @@ app.post('/signup', (req, res) => {
             }
         })
         .then(data => {
+            userId = data.user.uid
             return data.user.getIdToken();
         })
-        .then(token => {
+        .then(idToken => {
+            token = idToken
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId,
+            };
+            return db.doc(`/user/${newUser.handle}`).set(userCredentials)
+        })
+        .then(() => {
             return res.status(201).json({ token });
         })
         .catch(err => {
             console.error(err);
-            return res.status(500).json({ error: err.code })
+            if(err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({ email: 'Email is already in use'});
+            }else{
+                return res.status(500).json({ error: err.code })
+            }
         })
 });
 

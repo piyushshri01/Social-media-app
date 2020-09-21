5bba8eec2d7d2d66a7c2f527712454088dbafa1f
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-21 15:39:05 +0530

    "install express and get & post scream method created"

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,54 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
- 
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
+    admin
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then(data => {
+            let screams = [];
+            data.forEach(doc => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt,
+                });
+            });
+            return res.json(screams);
+        })
+        .catch(err => console.log(err))
+})
+
+
+app.post('/scream', (req,res) => {
+    if(req.method !== 'POST'){
+        return res.status(400).json({ error: 'Method not llowed '});
+    }
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
+    };
+
+    admin.firestore()
+        .collection('screams')
+        .add(newScream)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created successfully`})
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong'});
+            console.log(err);
+            
+        })
+})
+
+exports.api = functions.https.onRequest(app); 

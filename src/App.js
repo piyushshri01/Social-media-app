e1c6cbd0d83471cbc9b78f32bfe3f1bdce793d4b
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-23 11:48:29 +0530

    "install react-router and init it"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+// Pages
+import Home from './pages/home'
+import Login from './pages/login'
+import Signup from './pages/signup'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </Router>
       </div>
     )
   }

fe4b30d6ca2e55409c4127449fa8fb9126c68bb6
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-23 16:50:33 +0530

    "install dayjs  and converted createdAt to fromNow time "

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 //MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -24,6 +26,7 @@ const styles = {
 
 class Scream extends Component {
     render() {
+        dayjs.extend(relativeTime)
         const { classes, scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount } } = this.props;
         return (
             <Card className={classes.card}>
@@ -37,8 +40,8 @@ class Scream extends Component {
                     variant="h5" 
                     component={Link} 
                     to={`/users/${userHandle}`} 
-                    color="Primary">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                    color="primary">{userHandle}</Typography>
+                    <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1" >{body}</Typography>
                 </CardContent> 
             </Card>

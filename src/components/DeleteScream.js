3e9476adcea5fbbf4d21d0c93f15129fda22cd75
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-26 11:51:49 +0530

    " Add Delete Button for delete a scream"

---
+++ src/components/DeleteScream.js
@@ -0,0 +1,82 @@
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import MyButton from '../util/MyButton';
+
+// MUI Stuff
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+
+// Redux Stuff
+import { connect } from 'react-redux'
+import { deleteScream } from '../redux/actions/dataActions';
+
+// styles
+import styles from '../util/theme';
+
+const style = {
+    ...styles,
+    deleteButton: {
+        position: 'absolute',
+        left: '90%',
+        top: '10%'
+    }
+}
+
+export class DeleteScream extends Component {
+    state = {
+        open: false
+    };
+    handleOpen = () => {
+        this.setState({ open: true })
+    }
+    handleClose = () => {
+        this.setState({ open: false })
+    }
+    deleteScream = () => {
+        this.props.deleteScream(this.props.screamId);
+        this.setState({ open: false });
+    }
+    render() {
+        const { classes } = this.props;
+        return (
+            <Fragment>
+                <MyButton 
+                tip = "Delete Scream"
+                onClick={this.handleOpen}
+                btnClassName={classes.deleteButton}>
+                    <DeleteOutline color="secondary"/>
+                </MyButton>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    fullWidth
+                    maxWidth="sm">
+                       <DialogTitle>
+                           Are You Sure You Want To Delete This Scream ?
+                        </DialogTitle> 
+                        <DialogActions>
+                            <Button onClick={this.handleClose} color="primary">
+                                Cancel
+                            </Button>
+                            <Button onClick={this.deleteScream} color="secondary">
+                                Delete
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+            </Fragment>
+        )
+    }
+}
+
+DeleteScream.propTypes = {
+    deleteScream: PropTypes.func.isRequired,
+    classes: PropTypes.object.isRequired,
+    screamId: PropTypes.string.isRequired
+}
+
+
+export default connect(null, { deleteScream })(withStyles(style)(DeleteScream));
--- src/components/Scream.js
+++ src/components/Scream.js
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
+import DeleteScream from './DeleteScream';
 
 //MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -22,6 +23,7 @@ import { likeScream, unlikeScream } from '../redux/actions/dataActions';
 
 const styles = {
     card: {
+        position: 'relative',
         display: 'flex',
         marginBottom: 20,
     },
@@ -67,6 +69,10 @@ class Scream extends Component {
                 </MyButton>
             ) 
         );
+
+        const deleteButton = authenticated && userHandle === handle ? (
+            <DeleteScream screamId={screamId}/>
+        ) : null;
         return (
             <Card className={classes.card}>
                 <CardMedia 
@@ -80,6 +86,7 @@ class Scream extends Component {
                     component={Link} 
                     to={`/users/${userHandle}`} 
                     color="primary">{userHandle}</Typography>
+                    {deleteButton}
                     <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1" >{body}</Typography>
                     {likeButton}
